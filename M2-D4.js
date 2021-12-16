21909597f5170a032f6ebb79f630b78e8d2490e5
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2021-12-16 16:45:10 +0100

    Initial commit

---
+++ M2-D4.js
@@ -0,0 +1,35 @@
+function createPlayer() {
+    let name = document.createElement("li");
+    name.innerHTML = document.getElementById("text").value;
+    document.getElementById("waiting").appendChild(name)
+  }
+
+
+
+
+
+
+
+
+  function createTeams(){
+    document.getElementById("number")
+    
+    
+          
+          
+      
+      document.getElementById("waiting")
+      
+      
+         document.createElement("ul")
+          document.getElementById("grid").appendChild("ul")
+      }
+       
+
+
+  let random = Math.floor(Math.random() * 2)
+function assign(){
+    document.getElementById("waiting")
+    while (random = 0)
+}
+  
