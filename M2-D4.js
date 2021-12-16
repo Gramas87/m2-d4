4c04dcdeeafb56269262d0695dc348c28d42e89f
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2021-12-16 16:57:22 +0100

    Update M2-D4.js

--- M2-D4.js
+++ M2-D4.js
@@ -27,9 +27,14 @@ function createPlayer() {
        
 
 
-  let random = Math.floor(Math.random() * 2)
+  const random = Math.floor(Math.random() * 2)
 function assign(){
     document.getElementById("waiting")
-    while (random = 0)
+    document.getElementById("team1")
+    document.getElementById("team2")
+    document.getElementById("team3")
+    while (random = 0) return team1.push(waiting[0])
+    while (random = 1) return team2.push(waiting[0])
+    while (random = 3) return team3.push(waiting[0])
 }
   
