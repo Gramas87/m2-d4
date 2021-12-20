17c83783e1bd2605db388f1710e1dabda95b2d8b
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2021-12-20 08:54:47 +0100

    changes

--- M2-D4.js
+++ M2-D4.js
@@ -1,3 +1,4 @@
+
 function createPlayer() {
     let name = document.createElement("li");
     name.innerHTML = document.getElementById("text").value;
@@ -9,32 +10,22 @@ function createPlayer() {
 
 
 
-
-
+  const teamNumbers = document.querySelector("number").value
+// cant understand why it wont work
   function createTeams(){
-    document.getElementById("number")
+    
+    for (let i = 0; index < teamNumbers.value; i++) {
+      team=document.createElement("ul")
+      }
+      document.getElementsByTagName("body").appendChild(team)
     
     
           
           
       
-      document.getElementById("waiting")
       
-      
-         document.createElement("ul")
-          document.getElementById("grid").appendChild("ul")
       }
        
 
 
-  const random = Math.floor(Math.random() * 2)
-function assign(){
-    document.getElementById("waiting")
-    document.getElementById("team1")
-    document.getElementById("team2")
-    document.getElementById("team3")
-    while (random = 0) return team1.push(waiting[0])
-    while (random = 1) return team2.push(waiting[0])
-    while (random = 3) return team3.push(waiting[0])
-}
   
